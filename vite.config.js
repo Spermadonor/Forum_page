65d8a6de954ec6eed6191cdee4084d0b760aa769
Sergey <sen.ser@mail.ru>
2025-01-23 00:47:09 +0300

    Добавлен pug и вторая страница

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,22 @@
 import { defineConfig } from 'vite';
+import pugPlugin from "vite-plugin-pug"
+
+const options = { pretty: true } // FIXME: pug pretty is deprecated!
+const locals = { name: "My Pug" }
 
 export default defineConfig({
   base: '/Forum_page/',
+
+  plugins: [pugPlugin(options, locals)],
 /*   build: {
     cssMinify: false,
   }, */
+  build: {
+    rollupOptions: {
+      input: {
+        main: "./index.html",
+        messages: "./messages.html"
+      }
+    }
+  }
 });
