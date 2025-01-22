92d7dd54e9fca16f1c35502521062175da45d6fc
Sergey <sen.ser@mail.ru>
2025-01-23 01:13:45 +0300

    Правка vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite';
 import pugPlugin from "vite-plugin-pug"
 
-const options = { pretty: true } // FIXME: pug pretty is deprecated!
-const locals = { name: "My Pug" }
+const options = { pretty: true }
 
 export default defineConfig({
   base: '/Forum_page/',
 
-  plugins: [pugPlugin(options, locals)],
-/*   build: {
-    cssMinify: false,
-  }, */
+  plugins: [pugPlugin(options)],
   build: {
     rollupOptions: {
       input: {
